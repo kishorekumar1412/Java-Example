66fd7eabcc42b849856bd7b5fa708f60237c09ad
kishorekumar1412 <kishorkumar14121997@gmail.com>
2022-11-28 18:32:19 +0530

    new Commit

---
+++ Addition.java
@@ -0,0 +1,14 @@
+import java.lang.String;
+public class Addition{
+public static void main(String[] args){
+int a = 10;
+int b = 15;
+int c = add(a,b);
+System.out.println("The sumof a and b is:" +c);
+}
+public static int add(int n1, int n2){
+int s;
+s = n2+n2;
+return s;
+}
+}
